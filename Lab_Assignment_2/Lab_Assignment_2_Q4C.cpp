1736a606a9577c54fe8c3d6fa6e0e617292f54f1
Sudhanshu098-pro <sudhanshu.23scse1040221@galgotiasuniversity.ac.in>
2026-08-31 18:52:45 +0530

    Q4C

---
+++ Lab_Assignment_2/Lab_Assignment_2_Q4C.cpp
@@ -0,0 +1,27 @@
+// (c) Write a program to delete all the vowels from the string.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    cout << "String without vowels -  ";
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+           str[i] != 'o' && str[i] != 'u' &&
+           str[i] != 'A' && str[i] != 'E' && str[i] != 'I' &&
+           str[i] != 'O' && str[i] != 'U')
+        {
+            cout << str[i];
+        }
+    }
+
+    return 0;
+}
