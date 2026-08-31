ecdf737f987abbdfc081b31e212918fb8d1b823a
Sudhanshu098-pro <sudhanshu.23scse1040221@galgotiasuniversity.ac.in>
2026-08-31 18:55:03 +0530

    Q4D

---
+++ Lab_Assignment_2/Lab_Assignment_2_Q4D.cpp
@@ -0,0 +1,37 @@
+// (d) Write a program to sort the strings in alphabetical order
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string arr[5];
+
+    cout << "Enter 5 strings:" << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4 - i; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    cout << "Strings in alphabetical order:" << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
